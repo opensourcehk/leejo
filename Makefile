0b3e5864fead90d23703708610ce4d37ac5ca2e6
Koala Yeung <koalay@gmail.com>
2014-06-24 23:18:25 +0800

    Added integration test

--- Makefile
+++ Makefile
@@ -8,8 +8,14 @@ run: build
 
 build: bin/leejo_server
 
+check: bin/integration_test
+	bin/integration_test
+
 bin/leejo_server:
 	cd src; make GOPATH="${GOPATH}" BIN="${BIN}" build
+
+bin/integration_test:
+	cd tests; make GOPATH="${GOPATH}" BIN="${BIN}" build
 
 clean:
 	rm -Rf bin/*
---
+++ tests/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jmcvetta/napping"
+)
+
+type Resp struct {
+	Status string        `json:"status"`
+	Result []interface{} `json:"result"`
+}
+
+func testUser() (err error) {
+
+	var result Resp
+	var resp *napping.Response
+	var resultNum int
+
+	p := napping.Params{}
+
+	// create a user
+	resp, err = napping.Post("http://localhost:8080/api.v1/user",
+		&map[string]string{
+			"username": "Tester", // TODO: use uuid
+			"gender":   "F",
+		}, &result, nil)
+
+	resultNum = len(result.Result)
+	if resultNum != 1 {
+		return fmt.Errorf("Bad response in create user. "+
+			"There are %d results (expecting 1)",
+			resultNum)
+	}
+	fmt.Printf("Success creating user\n")
+
+	// for debug only
+	for i := 0; i < resultNum; i++ {
+		user := result.Result[i].(User)
+		fmt.Printf("User: %#v\n", user)
+	}
+	// debug help end
+
+	// retrieve the user
+	resp, err = napping.Get("http://localhost:8080/api.v1/user/1",
+		&p, &result, nil)
+	if err != nil {
+		return
+	}
+	if resp.Status() == 404 {
+		err = fmt.Errorf("I don't know why")
+		return
+	}
+	fmt.Printf("Integration test %d\n", resp.Status())
+	return
+}
+
+func main() {
+	err := testUser()
+	if err != nil {
+		panic(err)
+	}
+}
