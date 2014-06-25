f9ed87d42bfc0b604e5e121965f7dfbc166fa084
Koala Yeung <koalay@gmail.com>
2014-06-25 22:13:40 +0800

    Data structure change
    In order to fit in a RESTful paradigm, it is hard to handle skills
    and interests in the previous data structure design. It is much
    easier to identify a user-skill or user-interest relation with
    a relation id. And it is more expansible that way.

--- data/leejo_schema.sql
+++ data/leejo_schema.sql
@@ -9,14 +9,16 @@ create table leejo_user (
   primary key (user_id)
 );
 
-create table leejo_skill (
+create table leejo_user_skill (
+  user_skill_id serial,
   user_id integer not null default 0,
   skill_name varchar(255) not null default '',
-  primary key (user_id, skill_name)
+  primary key (user_skill_id)
 );
 
-create table leejo_interest (
+create table leejo_user_interest (
+  user_interest_id serial,
   user_id integer default 0,
   interest_name varchar(255) not null default '',
-  primary key (user_id, interest_name)
+  primary key (user_interest_id)
 );
