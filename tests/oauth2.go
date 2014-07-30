ce14c9723690bcf3754ad13ded2d2c4ef33d839c
Koala Yeung <koalay@gmail.com>
2014-07-30 18:05:12 +0800

    Integration test triggers authentication callback

--- tests/oauth2.go
+++ tests/oauth2.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func testAuth() (auth interface{}, err error) {
+	log.Printf("Test Auth\n")
+	_, err =
+		http.Get("http://localhost:8080/oauth2/authorize?response_type=code&client_id=testing")
 	return
 }
