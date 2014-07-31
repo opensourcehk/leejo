62b1ff4f8f21365ec257757246453102e0093bde
Koala Yeung <koalay@gmail.com>
2014-08-01 00:37:40 +0800

    Improve logging in token endpoint handler

--- src/oauth2/auth.go
+++ src/oauth2/auth.go
@@ -26,18 +26,22 @@ func Bind(authPath string, sessPtr *db.Database) {
 			ar.Authorized = true
 			oauth2.FinishAuthorizeRequest(resp, r, ar)
 		}
-		log.Printf("OAuth2 Authorize Response: %s", resp)
+		log.Printf("OAuth2 Authorize Response: %#v", resp)
 		osin.OutputJSON(resp, w, r)
 	})
 	http.HandleFunc(authPath+"/token", func(w http.ResponseWriter, r *http.Request) {
 		resp := oauth2.NewResponse()
 		if ar := oauth2.HandleAccessRequest(resp, r); ar != nil {
+			log.Printf("Access successful")
 			// TODO:
 			// 1. check the auth code, client id and secret (or is it checked already?)
 			// 2. if checking pass, generate and return token (or is it handled already?)
 			ar.Authorized = true
 			oauth2.FinishAccessRequest(resp, r, ar)
+		} else {
+			log.Printf("Internal Error: %s", resp.InternalError.Error())
 		}
+		log.Printf("OAuth2 Token Response: %#v", resp)
 		osin.OutputJSON(resp, w, r)
 	})
 
