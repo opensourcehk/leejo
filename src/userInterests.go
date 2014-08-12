4251a5bd347458522dbefbeec4123d0d8abe00a5
Koala Yeung <koalay@gmail.com>
2014-08-13 00:25:43 +0800

    Reimplement userSkill and userInterest
    Use the new REST service structure

--- src/userInterests.go
+++ src/userInterests.go
@@ -1,186 +1,79 @@
 package main
 
 import (
-	"encoding/json"
 	"github.com/RangelReale/osin"
 	"github.com/gorilla/pat"
-	"io/ioutil"
 	"leejo/data"
-	"log"
+	"leejo/service"
+	"leejo/service/upperio"
 	"net/http"
-	"strconv"
 	"upper.io/db"
 )
 
+// return a typical CURD service for User
+type UserInterestRest struct {
+	Db db.Database
+}
+
+// path which include entity specific information
+func (h *UserInterestRest) SubPath() string {
+	return "{id:[0-9]+}"
+}
+
+// allocate storage service for CURD operations of user
+func (h *UserInterestRest) Service(r *http.Request) service.Service {
+	// the content of service would be database specific
+	// but the interface of service would be generic
+	return &upperio.Service{
+		Db:       h.Db,
+		CollName: "leejo_user_interest",
+		IdSetterFunc: func(id upperio.Id, e service.EntityPtr) (err error) {
+			u := e.(*data.UserInterest)
+			u.UserInterestId = id.(int64)
+			return
+		},
+		KeyCondFunc: func(c service.Context) db.Cond {
+			return db.Cond{
+				"user_id": c.ParentKey,
+				"user_interest_id": c.Key,
+			}
+		},
+		ParentCondFunc: func(c service.Context) db.Cond {
+			return db.Cond{
+				"user_id": c.ParentKey,
+			}
+		},
+	}
+}
+
+// translate an http request into a query context
+func (h *UserInterestRest) Context(r *http.Request) service.Context {
+	return service.Context{
+		Key:       r.URL.Query().Get(":id"),
+		ParentKey: r.URL.Query().Get(":user_id"),
+		Cond: &service.BasicListCond{
+			Limit:  20,
+			Offset: 0,
+		},
+	}
+}
+
+// allocate an entity and return the address
+func (h *UserInterestRest) Entity() service.EntityPtr {
+	return &data.UserInterest{}
+}
+
+// allocate a slice of entity and return the address
+func (h *UserInterestRest) EntityList() service.EntityListPtr {
+	return &[]data.UserInterest{}
+}
+
+// create user CURD interface with pat
 func bindUserInterests(path string, osinServer *osin.Server, sessPtr *db.Database, r *pat.Router) {
 	sess := *sessPtr
-	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
-		userInterestsCol, err := sess.Collection("leejo_user_interest")
-		if err != nil {
-			panic(err)
-		}
 
-		// retrieve all userInterests of the user_id
-		user_id := r.URL.Query().Get(":user_id")
-		res := userInterestsCol.Find(db.Cond{
-			"user_id": user_id,
-		})
-		var userInterests []data.UserInterest
-		err = res.All(&userInterests)
-		if err != nil {
-			panic(err)
-		}
-
-		json.NewEncoder(w).Encode(data.Resp{
-			Status: "OK",
-			Result: userInterests,
-		})
-	})
-	r.Get(path+"/{id:[0-9]+}", func(w http.ResponseWriter, r *http.Request) {
-		userInterestsCol, err := sess.Collection("leejo_user_interest")
-		if err != nil {
-			panic(err)
-		}
-
-		// retrieve all userInterests of the user_id and id(s)
-		id := r.URL.Query().Get(":id")
-		user_id := r.URL.Query().Get(":user_id")
-		res := userInterestsCol.Find(db.Cond{
-			"user_interest_id": id,
-			"user_id":          user_id,
-		})
-		var userInterests []data.UserInterest
-		err = res.All(&userInterests)
-		if err != nil {
-			panic(err)
-		}
-
-		json.NewEncoder(w).Encode(data.Resp{
-			Status: "OK",
-			Result: userInterests,
-		})
-	})
-	r.Post(path, func(w http.ResponseWriter, r *http.Request) {
-
-		userInterest := data.UserInterest{}
-		bytes, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			log.Printf("Error reading request: ", err)
-			w.WriteHeader(500)
-			return
-		}
-
-		err = json.Unmarshal(bytes, &userInterest)
-		if err != nil {
-			log.Printf("Error JSON Unmarshal: ", err)
-			w.WriteHeader(500)
-			return
-		}
-		log.Printf("Request %#v", userInterest)
-
-		user_id := r.URL.Query().Get(":user_id")
-		inputUserId, err := strconv.ParseInt(user_id, 10, 64)
-		if err != nil {
-			panic(err)
-		}
-		userInterest.UserId = inputUserId // force to use URL's user id
-
-		userInterestsCol, err := sess.Collection("leejo_user_interest")
-		if err != nil {
-			panic(err)
-		}
-
-		// add the user to user collection
-		userId, err := userInterestsCol.Append(userInterest)
-		if err != nil {
-			panic(err)
-		}
-		userInterest.UserInterestId = userId.(int64)
-
-		json.NewEncoder(w).Encode(data.Resp{
-			Status: "OK",
-			Result: []data.UserInterest{userInterest},
-		})
-	})
-	r.Put(path+"/{id:[0-9]+}", func(w http.ResponseWriter, r *http.Request) {
-
-		userInterest := data.UserInterest{}
-		bytes, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			log.Printf("Error reading request: ", err)
-			w.WriteHeader(500)
-			return
-		}
-
-		err = json.Unmarshal(bytes, &userInterest)
-		if err != nil {
-			log.Printf("Error JSON Unmarshal: ", err)
-			w.WriteHeader(500)
-			return
-		}
-		log.Printf("Request %#v", userInterest)
-
-		var userInterests []data.UserInterest
-		userInterestsCol, err := sess.Collection("leejo_user_interest")
-		if err != nil {
-			panic(err)
-		}
-
-		id := r.URL.Query().Get(":id")
-		user_id := r.URL.Query().Get(":user_id")
-		res := userInterestsCol.Find(db.Cond{
-			"user_interest_id": id,
-			"user_id":          user_id,
-		})
-
-		// update the user
-		err = res.Update(userInterest)
-		if err != nil {
-			panic(err)
-		}
-
-		// retrieve the just updated record from database
-		res = userInterestsCol.Find(db.Cond{
-			"user_interest_id": id,
-			"user_id":          user_id,
-		})
-		err = res.All(&userInterests)
-		if err != nil {
-			panic(err)
-		}
-
-		json.NewEncoder(w).Encode(data.Resp{
-			Status: "OK",
-			Result: userInterests,
-		})
-	})
-	r.Delete(path+"/{id:[0-9]+}", func(w http.ResponseWriter, r *http.Request) {
-		userInterestsCol, err := sess.Collection("leejo_user_interest")
-		if err != nil {
-			panic(err)
-		}
-
-		// retrieve all userInterests of id(s)
-		id := r.URL.Query().Get(":id")
-		user_id := r.URL.Query().Get(":user_id")
-		res := userInterestsCol.Find(db.Cond{
-			"user_interest_id": id,
-			"user_id":          user_id,
-		})
-		var userInterests []data.UserInterest
-		err = res.All(&userInterests)
-		if err != nil {
-			panic(err)
-		}
-
-		// TODO: if len(userInterests) == 0, return 404 error
-
-		// remove all results from database
-		err = res.Remove()
-		json.NewEncoder(w).Encode(data.Resp{
-			Status: "OK",
-			Result: userInterests,
-		})
-
-	})
+	h := UserInterestRest{
+		Db: sess,
+	}
+	RestOnPat(path, &h, r)
 }
--- src/userSkills.go
+++ src/userSkills.go
@@ -1,186 +1,79 @@
 package main
 
 import (
-	"encoding/json"
 	"github.com/RangelReale/osin"
 	"github.com/gorilla/pat"
-	"io/ioutil"
 	"leejo/data"
-	"log"
+	"leejo/service"
+	"leejo/service/upperio"
 	"net/http"
-	"strconv"
 	"upper.io/db"
 )
 
+// return a typical CURD service for User
+type UserSkillRest struct {
+	Db db.Database
+}
+
+// path which include entity specific information
+func (h *UserSkillRest) SubPath() string {
+	return "{id:[0-9]+}"
+}
+
+// allocate storage service for CURD operations of user
+func (h *UserSkillRest) Service(r *http.Request) service.Service {
+	// the content of service would be database specific
+	// but the interface of service would be generic
+	return &upperio.Service{
+		Db:       h.Db,
+		CollName: "leejo_user_skill",
+		IdSetterFunc: func(id upperio.Id, e service.EntityPtr) (err error) {
+			u := e.(*data.UserSkill)
+			u.UserSkillId = id.(int64)
+			return
+		},
+		KeyCondFunc: func(c service.Context) db.Cond {
+			return db.Cond{
+				"user_id": c.ParentKey,
+				"user_skill_id": c.Key,
+			}
+		},
+		ParentCondFunc: func(c service.Context) db.Cond {
+			return db.Cond{
+				"user_id": c.ParentKey,
+			}
+		},
+	}
+}
+
+// translate an http request into a query context
+func (h *UserSkillRest) Context(r *http.Request) service.Context {
+	return service.Context{
+		Key:       r.URL.Query().Get(":id"),
+		ParentKey: r.URL.Query().Get(":user_id"),
+		Cond: &service.BasicListCond{
+			Limit:  20,
+			Offset: 0,
+		},
+	}
+}
+
+// allocate an entity and return the address
+func (h *UserSkillRest) Entity() service.EntityPtr {
+	return &data.UserSkill{}
+}
+
+// allocate a slice of entity and return the address
+func (h *UserSkillRest) EntityList() service.EntityListPtr {
+	return &[]data.UserSkill{}
+}
+
+// create user CURD interface with pat
 func bindUserSkills(path string, osinServer *osin.Server, sessPtr *db.Database, r *pat.Router) {
 	sess := *sessPtr
-	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
-		userSkillsCol, err := sess.Collection("leejo_user_skill")
-		if err != nil {
-			panic(err)
-		}
 
-		// retrieve all userSkills of the user_id
-		user_id := r.URL.Query().Get(":user_id")
-		res := userSkillsCol.Find(db.Cond{
-			"user_id": user_id,
-		})
-		var userSkills []data.UserSkill
-		err = res.All(&userSkills)
-		if err != nil {
-			panic(err)
-		}
-
-		json.NewEncoder(w).Encode(data.Resp{
-			Status: "OK",
-			Result: userSkills,
-		})
-	})
-	r.Get(path+"/{id:[0-9]+}", func(w http.ResponseWriter, r *http.Request) {
-		userSkillsCol, err := sess.Collection("leejo_user_skill")
-		if err != nil {
-			panic(err)
-		}
-
-		// retrieve all userSkills of the user_id and id(s)
-		id := r.URL.Query().Get(":id")
-		user_id := r.URL.Query().Get(":user_id")
-		res := userSkillsCol.Find(db.Cond{
-			"user_skill_id": id,
-			"user_id":       user_id,
-		})
-		var userSkills []data.UserSkill
-		err = res.All(&userSkills)
-		if err != nil {
-			panic(err)
-		}
-
-		json.NewEncoder(w).Encode(data.Resp{
-			Status: "OK",
-			Result: userSkills,
-		})
-	})
-	r.Post(path, func(w http.ResponseWriter, r *http.Request) {
-
-		userSkill := data.UserSkill{}
-		bytes, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			log.Printf("Error reading request: ", err)
-			w.WriteHeader(500)
-			return
-		}
-
-		err = json.Unmarshal(bytes, &userSkill)
-		if err != nil {
-			log.Printf("Error JSON Unmarshal: ", err)
-			w.WriteHeader(500)
-			return
-		}
-		log.Printf("Request %#v", userSkill)
-
-		user_id := r.URL.Query().Get(":user_id")
-		inputUserId, err := strconv.ParseInt(user_id, 10, 64)
-		if err != nil {
-			panic(err)
-		}
-		userSkill.UserId = inputUserId // force to use URL's user id
-
-		userSkillsCol, err := sess.Collection("leejo_user_skill")
-		if err != nil {
-			panic(err)
-		}
-
-		// add the user to user collection
-		userId, err := userSkillsCol.Append(userSkill)
-		if err != nil {
-			panic(err)
-		}
-		userSkill.UserSkillId = userId.(int64)
-
-		json.NewEncoder(w).Encode(data.Resp{
-			Status: "OK",
-			Result: []data.UserSkill{userSkill},
-		})
-	})
-	r.Put(path+"/{id:[0-9]+}", func(w http.ResponseWriter, r *http.Request) {
-
-		userSkill := data.UserSkill{}
-		bytes, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			log.Printf("Error reading request: ", err)
-			w.WriteHeader(500)
-			return
-		}
-
-		err = json.Unmarshal(bytes, &userSkill)
-		if err != nil {
-			log.Printf("Error JSON Unmarshal: ", err)
-			w.WriteHeader(500)
-			return
-		}
-		log.Printf("Request %#v", userSkill)
-
-		var userSkills []data.UserSkill
-		userSkillsCol, err := sess.Collection("leejo_user_skill")
-		if err != nil {
-			panic(err)
-		}
-
-		id := r.URL.Query().Get(":id")
-		user_id := r.URL.Query().Get(":user_id")
-		res := userSkillsCol.Find(db.Cond{
-			"user_skill_id": id,
-			"user_id":       user_id,
-		})
-
-		// update the user
-		err = res.Update(userSkill)
-		if err != nil {
-			panic(err)
-		}
-
-		// retrieve the just updated record from database
-		res = userSkillsCol.Find(db.Cond{
-			"user_skill_id": id,
-			"user_id":       user_id,
-		})
-		err = res.All(&userSkills)
-		if err != nil {
-			panic(err)
-		}
-
-		json.NewEncoder(w).Encode(data.Resp{
-			Status: "OK",
-			Result: userSkills,
-		})
-	})
-	r.Delete(path+"/{id:[0-9]+}", func(w http.ResponseWriter, r *http.Request) {
-		userSkillsCol, err := sess.Collection("leejo_user_skill")
-		if err != nil {
-			panic(err)
-		}
-
-		// retrieve all userSkills of id(s)
-		id := r.URL.Query().Get(":id")
-		user_id := r.URL.Query().Get(":user_id")
-		res := userSkillsCol.Find(db.Cond{
-			"user_skill_id": id,
-			"user_id":       user_id,
-		})
-		var userSkills []data.UserSkill
-		err = res.All(&userSkills)
-		if err != nil {
-			panic(err)
-		}
-
-		// TODO: if len(userSkills) == 0, return 404 error
-
-		// remove all results from database
-		err = res.Remove()
-		json.NewEncoder(w).Encode(data.Resp{
-			Status: "OK",
-			Result: userSkills,
-		})
-
-	})
+	h := UserSkillRest{
+		Db: sess,
+	}
+	RestOnPat(path, &h, r)
 }
